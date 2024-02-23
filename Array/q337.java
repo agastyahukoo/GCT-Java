84d84ab5ef26a7dadb29cd20426b4f07f7246bbb
Agastya Hukoo <agastyahukoo@gmail.com>
2024-02-23 16:30:41 +0530

    Array questions

---
+++ Array/q337.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class q337 {
+    public static void main(String[] args)
+    {
+        int[] a = {1, 2, 3, 4, 5};
+        for(int i=a.length-1;i>=0;i--)
+        {
+            System.out.print(a[i]+"\t");
+        }
+    }
+}
