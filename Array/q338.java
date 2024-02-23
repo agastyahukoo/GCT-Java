029c6a0a520b8202b70a455d888a8bf20ed82707
Agastya Hukoo <agastyahukoo@gmail.com>
2024-02-23 16:43:03 +0530

    New Questions

---
+++ Array/q338.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class q338 {
+    public static void main(String[] args)
+    {
+        String[] a = {"George", "Jensen","Satya"};
+        for(int i = a.length-1;i>=0;i--)
+        {
+            System.out.print(a[i]+"\t");
+        }
+    }
+}
---
+++ Array/q339.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class q339 {
+    public static void main(String[] args)
+    {
+        char[] a = {'A', 'E', 'I', 'O','U'};
+        for(int i = a.length-1;i>=0;i--)
+        {
+            System.out.print(a[i]+"\t");
+        }
+    }
+}
